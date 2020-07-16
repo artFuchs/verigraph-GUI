56446a994efdbebd3d42b4c847896a92359cbb4c
artFuchs <alfuchs17@gmail.com>
2020-07-15 22:54:09 -0300

    minor changes

--- src/GUI/Analysis/CriticalPairAnalysis.hs
+++ src/GUI/Analysis/CriticalPairAnalysis.hs
@@ -30,22 +30,6 @@ buildCpaWindow window = do
   resultBuffer <- new Gtk.TextBuffer []
   set textView [#buffer := resultBuffer]
 
-  Gtk.textBufferInsertAtCursor resultBuffer "⊂_ヽ\n" (-1)
-  Gtk.textBufferInsertAtCursor resultBuffer "　 ＼＼ ＿\n" (-1)
-  Gtk.textBufferInsertAtCursor resultBuffer "　　 ＼(　•_•) F\n" (-1)
-  Gtk.textBufferInsertAtCursor resultBuffer "　　　<　⌒ヽ A\n" (-1)
-  Gtk.textBufferInsertAtCursor resultBuffer "　　 /   　 へ＼ N\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "　  (  　　/  　＼＼T\n" (-1)  
-  Gtk.textBufferInsertAtCursor resultBuffer "　　 ﾚ 　ノ　　   ヽ_つA\n" (-1)  
-  Gtk.textBufferInsertAtCursor resultBuffer "　　/　/ S\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "　 /　/ T\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "　(　(ヽI\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "　|　|、＼C.\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "　| 丿 ＼ ⌒)\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "　| |　　) /\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "ノ )　　Lﾉ__\n" (-1) 
-  Gtk.textBufferInsertAtCursor resultBuffer "(／___\n" (-1) 
-
-
-
+  Gtk.textBufferInsertAtCursor resultBuffer "Critical Pair Analysis\n" (-1)
+  
   return (win, essentialCheckBtn, confCheckBtn, dependCheckBtn, execBtn, resultBuffer)
