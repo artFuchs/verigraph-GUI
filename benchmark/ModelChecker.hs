2744f2dabd2c0816fd83f1ebbe06bfdc13f7b221
artFuchs <alfuchs17@gmail.com>
2021-10-11 15:25:11 -0300

    changed the banchmark to use non-random models

--- benchmark/ModelChecker.hs
+++ benchmark/ModelChecker.hs
@@ -11,7 +11,7 @@ import qualified  Logic.Model                       as Logic
 main :: IO ()
 main = do
   expr <- return ( Logic.Temporal (Logic.A (Logic.F (Logic.Atom "a"))))
-  [m1,m2,m3,m4,m5,m6] <- mapM randomModel [1,10,100,1000,10000,100000]
+  [m1,m2,m3,m4,m5,m6] <- mapM notRandomModel [1,10,100,1000,10000,100000]
   defaultMain
     [ bgroup "SAT (AF a)"
       [ bench "1 state" $ whnf (Logic.satisfyExpr m1) expr
@@ -19,10 +19,14 @@ main = do
       , bench "100 states" $ whnf (Logic.satisfyExpr m3) expr
       , bench "1000 states" $ whnf (Logic.satisfyExpr m4) expr
       , bench "10000 states" $ whnf (Logic.satisfyExpr m5) expr
-      , bench "100000 states" $ whnf (Logic.satisfyExpr m6) expr
+      --, bench "100000 states" $ whnf (Logic.satisfyExpr m6) expr
       ]
     ]
   return ()
+
+
+
+
 
 
 
@@ -35,7 +39,7 @@ randomModel statesNum = do
     value <- randomRIO (1,100 :: Int)
     hasProposition <- return $ if i == 0 then False else value > 95 -- 5% of chances of having the proposition "a"
     return $ Logic.State i (if hasProposition then ["a"] else [])
-  levels <- setLevels statesNum []
+  levels <- setLevelsR statesNum []
   transitions <- if length levels > 1
     then do
       transitions <- forM [1..(length levels - 1)] $ \l -> do
@@ -51,10 +55,35 @@ randomModel statesNum = do
 
 -- given a number of states n, divide the states from 1 to n in levels
 -- each level have a random number of states
-setLevels :: Int -> [[Int]] -> IO [[Int]]
-setLevels 0 ls = return ls
-setLevels n [] = setLevels (n-1) [[1]]
-setLevels n ls = do
+setLevelsR :: Int -> [[Int]] -> IO [[Int]]
+setLevelsR 0 ls = return ls
+setLevelsR n [] = setLevelsR (n-1) [[1]]
+setLevelsR n ls = do
   nSts <- randomRIO (1,n)
   start <- return $ sum (concat ls)
-  setLevels (n-nSts) ((take nSts [start..]):ls)
+  setLevelsR (n-nSts) ((take nSts [start..]):ls)
+
+
+
+-- create a tree with a given number of states
+-- each level will have MIN(rest of states, 2 * #states of previous level)
+-- the leaf states will have the preposition "a"
+-- this is probably best
+notRandomModel :: Int -> IO (Logic.KripkeStructure String)
+notRandomModel statesNum = do
+  let levels = setLevels statesNum 0
+  let fstWithProp = head (last levels)
+  let states = map (\i -> Logic.State i (if i >= fstWithProp then ["a"] else [])) [1..statesNum]
+  let transitions = map (\i -> Logic.Transition (i-1) (floor $ (fromIntegral i)/2) i []) [2..statesNum]
+  let model = Logic.KripkeStructure states transitions
+  return model
+
+setLevels :: Int -> Int -> [[Int]]
+setLevels 0 _ = []
+setLevels n 0 = [1]:(setLevels (n-1) 1)
+setLevels n lv = l:(setLevels n'' (lv+1))
+  where
+    leng = round $ 2**(fromIntegral lv)
+    n' = n - leng
+    n'' = if n' < 0 then 0 else n'
+    l = take (min n leng) [leng..]
