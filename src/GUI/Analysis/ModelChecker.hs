4b2d19a0d3ad78fcfaa4505f2606112b8b2e2d68
artFuchs <alfuchs17@gmail.com>
2021-10-15 17:23:22 -0300

    divided modelChecker into ModelChecker and StateSpace

--- src/GUI/Analysis/ModelChecker.hs
+++ src/GUI/Analysis/ModelChecker.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables        #-}
 module GUI.Analysis.ModelChecker (
   buildStateSpaceBox
+, breadthFirstSearchIO
 ) where
 
 -- GTK modules
@@ -43,17 +44,18 @@ import qualified  Logic.Ctl                         as Logic
 import qualified  Logic.Model                       as Logic
 
 -- GUI modules
+import            GUI.Analysis.ModelChecker.StateSpace
 import            GUI.Data.Info hiding (empty)
 import qualified  GUI.Data.Info as Info
 import            GUI.Data.Nac
 import            GUI.Data.DiaGraph
 import            GUI.Data.GraphState
 import            GUI.Data.GraphicalInfo
+import            GUI.Dialogs
 import            GUI.Helper.BasicCanvasCallbacks
 import            GUI.Helper.GraphicalInfo
 import            GUI.Helper.GrammarMaker
 import            GUI.Helper.FilePath
-import            GUI.Dialogs
 import            GUI.Render.Render
 import            GUI.Render.GraphDraw
 
@@ -205,9 +207,7 @@ buildStateSpaceBox window store genStateSpaceItem focusedCanvas focusedStateIORe
 
 
 
-type NamedPredicate a b = (String, TypedGraphRule a b)
-type NamedProduction a b = (String, TypedGraphRule a b)
-type Space a b = SS.StateSpace (TGM.TypedGraphMorphism a b)
+
 
 
 
@@ -239,7 +239,7 @@ generateSSThread statusSpinner statusLabel canvas context timeMVar grammar state
   writeIORef constructThread (Just indicateThread)
 
   -- generate state
-  exploreStateSpace mconf statesNum grammar initialGraph ssMVar
+  exploreStateSpace mconf statesNum grammar initialGraph (Just ssMVar)
   return ()
 
 
@@ -360,96 +360,7 @@ generateStateSpaceVisualization stateSpace st = st''
 
 
 
--- state space generation -------------------------------------------------------------------------------------------------------------------------------------------------------
-exploreStateSpace :: DPO.MorphismsConfig (TGM.TypedGraphMorphism a b) -> Int -> DPO.Grammar (TGM.TypedGraphMorphism a b) -> TG.TypedGraph a b -> MVar (Space a b, Bool) -> IO (Int,Space a b)
-exploreStateSpace conf maxDepth grammar graph ssMVar =
-  {-# SCC "exploreStateSpace" #-}
-  let
-    (productions, predicates) =
-      splitPredicates (DPO.productions grammar)
 
-    getInitialId =
-      do
-        (idx, _) <- SS.putState graph
-        return idx
-
-    initialSpace =
-      SS.empty conf productions predicates
-  in do
-    (idx,space1) <- return $ SS.runStateSpaceBuilder getInitialId initialSpace
-    spacex <- breadthFirstSearchIO maxDepth [graph] space1 ssMVar
-    return (idx,spacex)
-
-
-{-| Runs a breadth-first search, starting with a initial state space.
-    The number of states explored are limited by the given number.
-    At each iteration of the search, updates the MVar ssMVar with the current result so that the user can stop at any moment.
--}
-breadthFirstSearchIO :: Int -> [(TG.TypedGraph a b)] -> Space a b -> MVar (Space a b, Bool) -> IO (Space a b)
-breadthFirstSearchIO 0 _ initialSpace ssMVar = return initialSpace
-breadthFirstSearchIO _ [] initialSpace ssMVar = return initialSpace
-breadthFirstSearchIO maxNum objs initialSpace ssMVar = do
-  let ((newNum, newObjs), state) = SS.runStateSpaceBuilder (bfsStep maxNum objs) initialSpace
-  putMVar ssMVar (state, null newObjs || newNum == 0)
-  breadthFirstSearchIO newNum newObjs state ssMVar
-
-
--- | Step of a breadth-first search.
-bfsStep :: forall morph. DPO.DPO morph => Int -> [Cat.Obj morph] -> SS.StateSpaceBuilder morph (Int,[Cat.Obj morph])
-bfsStep maxNum (obj:node_list) = do
-  (objIndex, _) <- SS.putState obj
-  successors <- expandSuccessors' maxNum (objIndex,obj)
-  let successors' = map (\(i,o,n) -> o) successors
-  return (maxNum - length successors', node_list ++ successors')
-
-
--- | Finds all transformations of the given state with the productions of the HLR system being explored, adding them to the state space.
--- Returns a list of the successor states as @(index, object, isNew)@, where @isNew@ indicates that the state was not present in the state space before.
--- limits the number of matches applications to @maxNum@
--- adapted from module Abstract.Rewriting.DPO.StateSpace
-expandSuccessors' :: forall morph. DPO.DPO morph => Int -> (Int, Cat.Obj morph) -> SS.StateSpaceBuilder morph [(Int, Cat.Obj morph, Bool)]
-expandSuccessors' maxNum (index, object) =
-  do
-    prods <- SS.getProductions
-    conf <- SS.getDpoConfig
-    matches <- return . concat . map (\(name,prod) -> map (\match -> (name,prod,match)) (DPO.findApplicableMatches conf prod object)) $ prods
-    (_,states) <- foldM expand (maxNum, []) matches
-    return states
-  where
-    expand :: (Int, [(Int, Cat.Obj morph, Bool)]) -> (String, Production morph, morph) -> SS.StateSpaceBuilder morph  (Int, [(Int, Cat.Obj morph, Bool)])
-    expand (n,l) (name,prod,match) =
-      if (n > 0) then
-        do
-          let object' = DPO.rewrite match prod
-          (index', isNew) <- SS.putState object'
-          SS.putTransition (index,index',name)
-          return $
-            if isNew then
-              (n-1,(index',object',isNew):l)
-            else
-              (n,l)
-      else
-        return (0,l)
-
-
-
-
--- | Separates the rules that change nothing (which are considered predicates)
--- from those that have some effect (which are considered productions).
--- definition taken as is from Verigraph CLI/ModelChecker.hs
-splitPredicates :: [(String, TypedGraphRule a b)] -> ([NamedProduction a b], [NamedPredicate a b])
-splitPredicates [] =
-  ([], [])
-
-splitPredicates ((name, rule) : rest) =
-  let
-    (productions, predicates) =
-      splitPredicates rest
-  in
-    if Cat.isIsomorphism (leftMorphism rule) && Cat.isIsomorphism (rightMorphism rule) then
-      (productions, (name, rule):predicates)
-    else
-      ((name, rule):productions, predicates)
 
 
 
