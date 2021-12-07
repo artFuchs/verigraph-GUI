1aab02b8590efe0dbc087e79f8267ed0315b39a0
artFuchs <alfuchs17@gmail.com>
2021-12-07 12:33:01 -0300

    updated help window

--- src/GUI/HelpWindow.hs
+++ src/GUI/HelpWindow.hs
@@ -8,6 +8,8 @@ import qualified GI.Gtk as Gtk
 import           Data.GI.Base
 import           Data.GI.Base.ManagedPtr (unsafeCastTo)
 
+import           Control.Monad
+
 import           Data.Maybe
 import           Data.Int
 import qualified Data.Text as T
@@ -25,42 +27,50 @@ buildHelpWindow = do
   textBuffer1 <- Gtk.builderGetObject builder "textbuffer1" >>= unsafeCastTo Gtk.TextBuffer . fromJust
   textBuffer2 <- Gtk.builderGetObject builder "textbuffer2" >>= unsafeCastTo Gtk.TextBuffer . fromJust
   textBuffer3 <- Gtk.builderGetObject builder "textbuffer3" >>= unsafeCastTo Gtk.TextBuffer . fromJust
-  
-  
-  let 
+  textBuffer4 <- Gtk.builderGetObject builder "textbuffer4" >>= unsafeCastTo Gtk.TextBuffer . fromJust
+
+  let
     applyTags :: Gtk.TextBuffer -> Int32 -> Int32 -> IO ()
     applyTags buffer ln maxln = do
         start <- Gtk.textBufferGetIterAtLine buffer ln
         end   <- Gtk.textBufferGetIterAtLine buffer ln
         _     <- Gtk.textIterForwardToLineEnd end
         startChar  <- Gtk.textIterGetChar start
-        
-        if startChar == '#' 
-            then do 
+
+        if startChar == '#'
+            then do
                 Gtk.textBufferApplyTagByName buffer "titleTag" start end
                 nextChar <- Gtk.textBufferGetIterAtLineOffset buffer ln 1
                 Gtk.textBufferDelete buffer start nextChar
             else return ()
-            
+
         if ln < maxln
             then applyTags buffer (ln+(1::Int32)) maxln
             else return ()
-                
-  textBuffer1LC <- Gtk.textBufferGetLineCount textBuffer1
-  applyTags textBuffer1 0 (textBuffer1LC-1)
-  
-  textBuffer2LC <- Gtk.textBufferGetLineCount textBuffer2
-  applyTags textBuffer2 0 (textBuffer2LC-1)
 
-  textBuffer3LC <- Gtk.textBufferGetLineCount textBuffer3
-  applyTags textBuffer3 0 (textBuffer3LC-1)
+  let tbuffers = [textBuffer1, textBuffer2, textBuffer3, textBuffer4]
+  forM_ tbuffers $ \tbuffer -> do
+    tbufferLC <- Gtk.textBufferGetLineCount tbuffer
+    applyTags tbuffer 0 (tbufferLC-1)
+
+  -- textBuffer1LC <- Gtk.textBufferGetLineCount textBuffer1
+  -- applyTags textBuffer1 0 (textBuffer1LC-1)
+  --
+  -- textBuffer2LC <- Gtk.textBufferGetLineCount textBuffer2
+  -- applyTags textBuffer2 0 (textBuffer2LC-1)
+  --
+  -- textBuffer3LC <- Gtk.textBufferGetLineCount textBuffer3
+  -- applyTags textBuffer3 0 (textBuffer3LC-1)
+  --
+  -- textBuffer4LC <- Gtk.textBufferGetLineCount textBuffer4
+  -- applyTags textBuffer4 0 (textBuffer4LC-1)
 
   on helpWindow #deleteEvent $ return $ do
       #hide helpWindow
       return True
 
   return helpWindow
-  
+
 
 buildAboutDialog :: IO ()
 buildAboutDialog = do
