381c513ab68ae42753aca35c4edb10e9953ad2a6
artFuchs <alfuchs17@gmail.com>
2022-03-07 20:10:01 -0300

    creating automaton for formulas

---
+++ src/Logic/Ltl.hs
@@ -0,0 +1,42 @@
+{-|
+Description : Syntax and model checking for LTL.
+
+Maintainer  : Arthur Lucena Fuchs <alfuchs@inf.ufrgs.br>
+Stability   : provisional
+-}
+module Logic.Ltl
+  (
+  -- * CTL Expressions
+    module Logic.Ltl.Base
+  , parseExpr
+
+  -- * Model checking
+  -- , check
+  , module Logic.Ltl.Semantics
+  ) where
+
+
+import           Logic.Ltl.Base
+import           Logic.Ltl.Parser
+import           Logic.Ltl.Semantics
+import           Logic.Model
+
+import Control.Monad
+
+example :: String -> IO ()
+example formula = do
+  let eExpr = parseExpr "" formula
+  case eExpr of
+    Left err -> putStrLn $ "Parsing error for formula " ++ formula ++ ":" ++ (show err)
+    Right expr -> do
+      let auto = exprAutomaton expr
+      putStrLn "states: "
+      forM_ (states auto) print
+      putStrLn "transitions: "
+      forM_ (transitions auto) print
+      return ()
+
+
+-- | Check if the given expression holds in the given state of the Kripke structure.
+-- check :: KripkeStructure String -> Expr -> Int -> Bool
+-- check model expr s0 = s0 `elem` satisfyExpr' model expr
--- src/Logic/Ltl/Semantics.hs
+++ src/Logic/Ltl/Semantics.hs
@@ -2,16 +2,98 @@ module Logic.Ltl.Semantics (
   rewriteExpr
 , closure
 , Closure
+, exprAutomaton
+, genTransitions
+, genStateIdPairs
 ) where
 
 import Logic.Ltl.Base
 import Logic.Ltl.Parser
+import qualified Logic.Model as Logic
 
+import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.List ((\\))
 
 
 type Closure = Set Expr
+
+
+-- | given a expression, generates an automaton for it
+exprAutomaton :: Expr -> Logic.KripkeStructure Expr
+exprAutomaton expr = Logic.KripkeStructure states transitions
+  where
+    transitions = genTransitions states
+    states = genStates expr
+
+
+genTransitions :: [Logic.State Expr] -> [Logic.Transition Expr]
+genTransitions states = transitions
+  where
+    transitions = zipWith (\i (s,t) -> Logic.Transition i s t []) [1..] idPairs
+    idPairs = genStateIdPairs states
+    others state = states \\ [state]
+
+
+genStateIdPairs :: [Logic.State Expr] -> [(Int, Int)]
+genStateIdPairs states = map (\(a,b) -> (Logic.elementId a, Logic.elementId b)) selected
+  where
+    selected = filter satisfyRestrictions allPairs
+    allPairs = [(a,b)| a <- states, b <- states]
+
+    satisfyRestrictions :: (Logic.State Expr, Logic.State Expr) -> Bool
+    satisfyRestrictions (Logic.State _ exprsA, Logic.State _ exprsB) =
+      transitionSatisfy exprsA exprsB
+
+    transitionSatisfy exprsA exprsB = and $ map satisfy exprsA
+      where
+        satisfy (Temporal(X e)) = e `elem` exprsB
+        satisfy (Not (Temporal (X e))) = (Not e) `elem` exprsB
+        satisfy e@(Temporal (U e1 e2)) =
+          if e2 `notElem` exprsA
+            then e `elem` exprsB
+            else True
+        satisfy e@(Not (Temporal (U e1 e2))) =
+          if e1 `elem` exprsA
+            then e `elem` exprsB
+            else True
+        satisfy _ = True
+
+
+genStates :: Expr -> [Logic.State Expr]
+genStates expr =
+  zipWith Logic.State [1..] (map Set.toList statesSets)
+  where
+    statesSets = genStatesSets (closure expr)
+
+genStatesSets :: Closure -> [Set Expr]
+genStatesSets clos = Set.toList respectingsets
+  where
+    respectingsets = Set.filter respectOps maxsubsets
+    maxsubsets = Set.filter (\s -> Set.size s == maxSize) consistentsets
+    maxSize = fromMaybe 0 $ Set.lookupMax (Set.map Set.size consistentsets)
+    consistentsets = Set.filter isConsistent powerset
+    powerset = Set.powerSet clos
+
+respectOps :: Set Expr -> Bool
+respectOps s = and . Set.toList $ Set.map respect s
+  where
+    respect (And e1 e2) = (e1 `Set.member` s) && (e2 `Set.member` s)
+    respect (Or e1 e2) = (e1 `Set.member` s) || (e2 `Set.member` s)
+    respect (Implies e1 e2) = ((e1 `Set.member` s) && (e2 `Set.member` s)) || (e1 `Set.notMember` s)
+    respect (Equiv e1 e2) = ((e1 `Set.member` s) && (e2 `Set.member` s)) ||
+                             ((e1 `Set.notMember` s) && (e2 `Set.notMember` s))
+    respect (Temporal (U e1 e2)) = respect (Or e1 e2)
+    respect (Not (Temporal (U e1 e2))) = (Not e2) `Set.member` s
+    respect _ = True
+
+isConsistent :: Set Expr -> Bool
+isConsistent s = and . Set.toList $ Set.map hasNoOpposite s
+  where
+    hasNoOpposite (Literal b) = Literal (not b) `Set.notMember` s
+    hasNoOpposite (Not e) = e `Set.notMember` s
+    hasNoOpposite e = (Not e) `Set.notMember` s
 
 
 
